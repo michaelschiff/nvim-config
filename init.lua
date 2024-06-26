72d525a5a228817e65db4cf4384e8cce1722a191
michaelschiff <schiff.michael@gmail.com>
2024-06-26 00:28:37 -0700

    lua lsp, playing with defining my own plugins, reformat init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "       -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
 -- disable netrw at the very start of your init.lua
@@ -25,158 +25,203 @@ vim.opt.number = true
 
 require("lazy").setup({
 	{
-	  "nvim-tree/nvim-tree.lua",
-	  version = "*",
-	  lazy = false,
-	  dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	  },
-	  config = function()
-	    require("nvim-tree").setup {}
-	    require("nvim-tree.api").tree.open()
-	  end,
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {}
+			require("nvim-tree.api").tree.open()
+		end,
 	},
-	{'nvim-tree/nvim-web-devicons', config = function() require('nvim-web-devicons').setup({}) end},
-	{'glepnir/nerdicons.nvim', cmd = 'NerdIcons', config = function() require('nerdicons').setup({}) end},
+	{ 'nvim-tree/nvim-web-devicons', config = function() require('nvim-web-devicons').setup({}) end },
+	{ 'glepnir/nerdicons.nvim',      cmd = 'NerdIcons',                                             config = function()
+		require('nerdicons').setup({}) end },
 	{
-	    'akinsho/bufferline.nvim',
-	    version = "*", 
-	    dependencies = 'nvim-tree/nvim-web-devicons',
-	    config = function()
-	      require("bufferline").setup({})
-	    end,
-        },
-	{'f-person/git-blame.nvim'},
+		'akinsho/bufferline.nvim',
+		version = "*",
+		dependencies = 'nvim-tree/nvim-web-devicons',
+		config = function()
+			require("bufferline").setup({})
+		end,
+	},
+	{ 'f-person/git-blame.nvim' },
 	{
-            "FabijanZulj/blame.nvim",
-            config = function()
-              require("blame").setup()
-            end,
-        },
-	{
-	    "neovim/nvim-lspconfig", 
-	    config = function() 
-		local lspconfig = require("lspconfig")
-		lspconfig.gopls.setup({
-		  settings = {
-		    gopls = {
-		      analyses = {
-			unusedparams = true,
-		      },
-		      staticcheck = true,
-		      gofumpt = true,
-		    },
-		  },
-		})
-		lspconfig.jdtls.setup({})
-		lspconfig.marksman.setup({})
-	    end,
+		"FabijanZulj/blame.nvim",
+		config = function()
+			require("blame").setup()
+		end,
 	},
 	{
-	    "hrsh7th/nvim-cmp",
-	    dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	    },
-	    config = function()
-		local cmp = require("cmp")
-		cmp.setup({
-		    snippet = {
-		      -- REQUIRED - you must specify a snippet engine
-		      expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-		      end,
-		    },
-		    window = {
-		      -- completion = cmp.config.window.bordered(),
-		      -- documentation = cmp.config.window.bordered(),
-		    },
-		    mapping = cmp.mapping.preset.insert({
-		      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-		      ['<C-Space>'] = cmp.mapping.complete(),
-		      ['<C-e>'] = cmp.mapping.abort(),
-		      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		    }),
-		    sources = cmp.config.sources({
-		      { name = 'nvim_lsp' },
-		      { name = 'vsnip' }, -- For vsnip users.
-		      -- { name = 'luasnip' }, -- For luasnip users.
-		      -- { name = 'ultisnips' }, -- For ultisnips users.
-		      -- { name = 'snippy' }, -- For snippy users.
-		    }, {
-		      { name = 'buffer' },
-		    })
-		  })
-	    end,
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.gopls.setup({
+				settings = {
+					gopls = {
+						analyses = {
+							unusedparams = true,
+						},
+						staticcheck = true,
+						gofumpt = true,
+					},
+				},
+			})
+			lspconfig.jdtls.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.lua_ls.setup({
+				on_init = function(client)
+					local path = client.workspace_folders[1].name
+					if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+						return
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend('force',
+						client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT'
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+								-- Depending on the usage, you might want to add additional paths here.
+								-- "${3rd}/luv/library"
+								-- "${3rd}/busted/library",
+							}
+							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+							-- library = vim.api.nvim_get_runtime_file("", true)
+						}
+					})
+				end,
+				settings = {
+					Lua = {
+						format = {
+							enable = true,
+							-- Put format options here
+							-- NOTE: the value should be String!
+							defaultConfig = {
+								indent_style = "space",
+								indent_size = "2",
+							}
+						},
+					}
+				}
+			})
+		end,
+	},
+	{ "windwp/nvim-autopairs",  config = function() require("nvim-autopairs").setup({}) end },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+					end,
+				},
+				window = {
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'vsnip' }, -- For vsnip users.
+					-- { name = 'luasnip' }, -- For luasnip users.
+					-- { name = 'ultisnips' }, -- For ultisnips users.
+					-- { name = 'snippy' }, -- For snippy users.
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+		end,
 	},
 	{
-	    "nvim-lualine/lualine.nvim",
-	    dependencies = { 'nvim-tree/nvim-web-devicons' },
-	    config = function()
-		require("lualine").setup({
-		  options = {
-		    icons_enabled = true,
-		    theme = 'auto',
-		    component_separators = { left = '', right = ''},
-		    section_separators = { left = '', right = ''},
-		    disabled_filetypes = {
-		      statusline = {},
-		      winbar = {},
-		    },
-		    ignore_focus = {},
-		    always_divide_middle = true,
-		    globalstatus = false,
-		    refresh = {
-		      statusline = 1000,
-		      tabline = 1000,
-		      winbar = 1000,
-		    }
-		  },
-		  sections = {
-		    lualine_a = {'mode'},
-		    lualine_b = {'branch', 'diff', 'diagnostics'},
-		    lualine_c = {'filename'},
-		    lualine_x = {'encoding', 'fileformat', 'filetype'},
-		    lualine_y = {'progress'},
-		    lualine_z = {'location'}
-		  },
-		  inactive_sections = {
-		    lualine_a = {},
-		    lualine_b = {},
-		    lualine_c = {'filename'},
-		    lualine_x = {'location'},
-		    lualine_y = {},
-		    lualine_z = {}
-		  },
-		  tabline = {},
-		  winbar = {},
-		  inactive_winbar = {},
-		  extensions = {}
-		})
-	    end,
-        },
+		"nvim-lualine/lualine.nvim",
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = 'auto',
+					component_separators = { left = '', right = '' },
+					section_separators = { left = '', right = '' },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					}
+				},
+				sections = {
+					lualine_a = { 'mode' },
+					lualine_b = { 'branch', 'diff', 'diagnostics' },
+					lualine_c = { 'filename' },
+					lualine_x = { 'encoding', 'fileformat', 'filetype' },
+					lualine_y = { 'progress' },
+					lualine_z = { 'location' }
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { 'filename' },
+					lualine_x = { 'location' },
+					lualine_y = {},
+					lualine_z = {}
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {}
+			})
+		end,
+	},
 	{
-	    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	    dependencies = { 'nvim-lua/plenary.nvim' },
-	    config = function()
-		local builtin = require('telescope.builtin')
-	        vim.keymap.set('n', '<C-f><C-f>', builtin.find_files, {})
-	        vim.keymap.set('n', '<C-n>', builtin.lsp_references, {})
-	        vim.keymap.set('n', '<C-j>', builtin.lsp_incoming_calls, {})
-		vim.keymap.set('n', '<C-m>', vim.lsp.buf.definition, {})
-		vim.keymap.set('n', '<C-o>', vim.lsp.buf.document_symbol, {})
-	    end,
-        },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{"mfussenegger/nvim-dap"},
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.6',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<C-f><C-f>', builtin.find_files, {})
+			vim.keymap.set('n', '<C-n>', builtin.lsp_references, {})
+			vim.keymap.set('n', '<C-j>', builtin.lsp_incoming_calls, {})
+			vim.keymap.set('n', '<C-m>', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<C-i>', vim.lsp.buf.implementation, {})
+			vim.keymap.set('n', '<C-o>', vim.lsp.buf.document_symbol, {})
+		end,
+	},
+	{ "nvim-treesitter/nvim-treesitter",                build = ":TSUpdate" },
+	{ "mfussenegger/nvim-dap" },
+	{ "ellisonleao/gruvbox.nvim",                       priority = 1000,                                                config = true },
+	{ dir = "/Users/michaelschiff/Documents/nvim-todo", config = function() require("todo").setup({ name = "michael" }) end },
 })
 
-vim.o.background = "light"  -- or "dark"
+vim.o.background = "light" -- or "dark"
 vim.cmd([[colorscheme gruvbox]])
-
-
